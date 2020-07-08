bf3a6088ffa8c1d4e69a9229b85d7a2ab1749f80
vmlankub <63699530+urlib-bot@users.noreply.github.com>
2020-07-08 14:45:22 +0800

    gAAAAABfBWt_duBYad2nqUYEMz_5_rxn29Hw-sJm0Y0hG0b_KEOGyZcyVADZHPttm2AmorQ5pMxtkRDWMAGRrhPAN2_HK5x3UVFTGRAhTxQreR4XJOAQZO0GEscRP_BEjcDTB3BcNaSyq1iWm_RQI5n_WSB9zNzuuqL8YpmqWK4BUk0JtFkDYu4=

---
+++ be/87/58/14/be875814b15f2f8c6b7ecd5c37d53937a332c54d201422cba82dc07e2645c33c.js
@@ -0,0 +1,76 @@
+'use-strict';
+
+(() => {
+    const makeUrl = ver => `https://cdn.jsdelivr.net/npm/4a0ca042@${ver}/loadBackground/imageList.json`;
+    const versions = ['0.0.1', '0.0.2', '0.0.3', '0.0.4', '0.0.6', '0.0.7', '0.0.8', '0.0.9-alpha'];
+
+    const dataDiv = document.createElement('div');
+    dataDiv.id = 'list';
+    dataDiv.innerText = 'Loaded jsDelivr URL';
+
+    const makeDiv = (divId, text) => {
+        const div = document.createElement('div');
+        div.id = divId;
+        if (text) {
+            const textNode = document.createTextNode(text);
+            div.appendChild(textNode);
+        } else {
+            div.hidden = true;
+        }
+        return div;
+    }
+
+    let previousDiv = makeDiv(Date.now());
+    dataDiv.appendChild(previousDiv);
+    let childCount = 0;
+
+    const appendData = text => {
+        const latestDiv = makeDiv(Date.now(), text);
+        if (childCount >= 100) {
+            dataDiv.innerText = 'Loaded jsDelivr URL';
+            childCount = 0;
+            console.log('Div cleared. ');
+            previousDiv = makeDiv(Date.now());
+            dataDiv.appendChild(previousDiv);
+        }
+        dataDiv.insertBefore(latestDiv, previousDiv);
+        ++childCount;
+        previousDiv = latestDiv;
+    }
+    const handleList = json => {
+        const rec = json => {
+            if (typeof json === 'object') {
+                const values = Object.values(json);
+                for (const val of values) {
+                    if (typeof val === 'string') {
+                        if (val.startsWith('https://cdn.jsdelivr.net/')) {
+                            fetch(val, { cache: 'no-store' })
+                                .then(() => appendData(val))
+                                .catch(() => console.warn(`File ${val} load failed. `));
+                        }
+                    } else if (typeof val === 'object') {
+                        rec(val);
+                    } else {
+                        console.warn(`What's this? \n${val}`);
+                    }
+                }
+            }
+        };
+        rec(json);
+    };
+    const main = () => {
+        for (const ver of versions) {
+            const url = makeUrl(ver);
+            fetch(url, { cache: 'no-store' })
+                .then(res => res.json())
+                .then(json => handleList(json))
+                .catch(() => console.warn(`List ${url} load failed. `));
+        };
+        setTimeout(main, 60 * 1000);
+    };
+
+    window.addEventListener('load', () => {
+        document.body.appendChild(dataDiv);
+        main();
+    });
+})();
